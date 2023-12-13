a11397c72a772e91c5cd13d3fc2d7ec18710d5be
Pushkar Kurhekar <pshkrh@gmail.com>
2023-12-13 00:26:19 -0500

    Update model_dir default

--- generate_pizza_image.py
+++ generate_pizza_image.py
@@ -2,8 +2,8 @@ from diffusers import DDPMPipeline
 from PIL import Image
 import torch
 
-if __name__ == '__main__':
-    model_dir = './ddpm-pizza-256-v3'
+if __name__ == "__main__":
+    model_dir = "./ddpm-pizza-128"
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -13,7 +13,9 @@ if __name__ == '__main__':
     num_images_to_generate = 1
 
     pipeline = DDPMPipeline.from_pretrained(model_dir, use_safetensors=True).to(device)
-    images = pipeline(batch_size=num_images_to_generate, generator=torch.manual_seed(0)).images
+    images = pipeline(
+        batch_size=num_images_to_generate, generator=torch.manual_seed(0)
+    ).images
 
     for idx, img in enumerate(images, start=1):
-        img.save(f'pizza-generated-{idx}.jpg')
+        img.save(f"pizza-generated-{idx}.jpg")
